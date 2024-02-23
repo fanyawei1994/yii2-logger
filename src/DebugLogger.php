195b4e9b9f2acdd561aef4ac0c152b888395b2f0
fanyawei <fanyawei>
2024-02-23 16:03:33 +0800

    init

---
+++ src/DebugLogger.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: fanyawei
+ * Date: 2024/2/23 16:01
+ */
+
+namespace Fanyawei\Yii2Logger;
+
+class DebugLogger
+{
+
+    public static function test()
+    {
+        return 'test';
+    }
+}
